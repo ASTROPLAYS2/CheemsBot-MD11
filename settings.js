980150f14970a3fdf6c3bf34eb0eb6a52371f7ea
ASTROPLAYS2 <hbwabot0@gmail.com>
2025-01-24 15:37:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 //base by DGXeon
 //re-upload? recode? copy code? give credit ya :)
 //YouTube: @DGXeon
-//Instagram: unicorn_xeon13
+//Instagram: ka_ning_ka_kal_dawn
 //Telegram: t.me/xeonbotinc
 //GitHub: @DGXeon
 //WhatsApp: +916909137213
@@ -19,19 +19,19 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = '꧁۝AstrO۝꧂' //ur bot name
+global.ownernumber = '917085345102' //ur owner number
+global.ownername = '꧁۝AstrO۝꧂' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "꧁۝AstrO۝꧂\n\n+917085345102"
+global.creator = "917085345102@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["917085345102"] // Premium User
 global.hituet = 0
 
 //bot sett
